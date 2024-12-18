95252a556a245e49bb464c61177e1f43ace08d40
Muhyiddin <muhyiddinmahfudz20@gmail.com>
2024-12-18 10:44:04 +0700

    fixing crud

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -13,7 +13,8 @@ class ClientController extends Controller
 {
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
+
+        $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:250',
             'slug' => 'required|string|max:100|unique:my_client',
             'is_project' => 'in:0,1',
@@ -24,20 +25,33 @@ class ClientController extends Controller
             'phone_number' => 'nullable|string|max:50',
             'city' => 'nullable|string|max:50',
         ]);
+
+        $filters = [
+            'name'=>'trim|escape',
+            'slug'=>'trim|escape',
+            'address'=>'trim|escape',
+            'email'=>'trim|escape',
+            'password'=>'trim|escape',
+            'phone_number'=>'trim|escape',
+            'city'=>'trim|escape',
+        ];
+        $sanitizer  = new Sanitizer($request->all(), $filters);
+        $attrclean=$sanitizer->sanitize();
+
         
         $clientLogo = $request->file('client_logo')->store('client_logos', 's3');
         $clientLogoUrl = Storage::disk('s3')->url($clientLogo);
 
         $client = MyClient::create([
-            'name' => $validatedData['name'],
-            'slug' => $validatedData['slug'],
-            'is_project' => $validatedData['is_project'] ?? '0',
-            'self_capture' => $validatedData['self_capture'] ?? '1',
-            'client_prefix' => $validatedData['client_prefix'],
+            'name' => $attrclean['name'],
+            'slug' => $attrclean['slug'],
+            'is_project' => $attrclean['is_project'] ?? '0',
+            'self_capture' => $attrclean['self_capture'] ?? '1',
+            'client_prefix' => $attrclean['client_prefix'],
             'client_logo' => $clientLogoUrl,
-            'address' => $validatedData['address'] ?? null,
-            'phone_number' => $validatedData['phone_number'] ?? null,
-            'city' => $validatedData['city'] ?? null,
+            'address' => $attrclean['address'] ?? null,
+            'phone_number' => $attrclean['phone_number'] ?? null,
+            'city' => $attrclean['city'] ?? null,
         ]);
 
         Redis::set($client->slug, json_encode($client));
@@ -68,16 +82,29 @@ class ClientController extends Controller
             return response()->json(['message' => 'Client not found'], 404);
         }
 
-        $validatedData = $request->validate([
-            'name' => 'nullable|string|max:250',
-            'is_project' => 'nullable|in:0,1',
-            'self_capture' => 'nullable|in:0,1',
-            'client_prefix' => 'nullable|string|max:4',
-            'client_logo' => 'nullable|image|mimes:jpg,jpeg,png,gif|max:2048',
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:250',
+            'slug' => 'required|string|max:100|unique:my_client',
+            'is_project' => 'in:0,1',
+            'self_capture' => 'in:0,1',
+            'client_prefix' => 'required|string|max:4',
+            'client_logo' => 'required|image|mimes:jpg,jpeg,png,gif|max:2048',
             'address' => 'nullable|string',
             'phone_number' => 'nullable|string|max:50',
             'city' => 'nullable|string|max:50',
         ]);
+
+        $filters = [
+            'name'=>'trim|escape',
+            'slug'=>'trim|escape',
+            'address'=>'trim|escape',
+            'email'=>'trim|escape',
+            'password'=>'trim|escape',
+            'phone_number'=>'trim|escape',
+            'city'=>'trim|escape',
+        ];
+        $sanitizer  = new Sanitizer($request->all(), $filters);
+        $attrclean=$sanitizer->sanitize();
 
         if ($request->hasFile('client_logo')) {
             $clientLogo = $request->file('client_logo')->store('client_logos', 's3');
@@ -85,7 +112,7 @@ class ClientController extends Controller
             $client->client_logo = $clientLogoUrl;
         }
 
-        $client->update($validatedData);
+        $client->update($attrclean);
 
         Redis::del($slug);
         Redis::set($slug, json_encode($client));
